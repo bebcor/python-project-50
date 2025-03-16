daf07b7d4f40987b560a15819fcaf7635bdafc0f
иван шулубин <shulubin.ia@gmail.com>
2025-03-16 20:45:53 +0300

    Добавлено чтение файлов и парсинг

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,19 +1,25 @@
 import argparse
+import json
+
+
+def read_and_parse(file_path):
+    with open(file_path, 'r') as result_file:
+        return json.load(result_file)
 
 
 def main():
     parser = argparse.ArgumentParser(
-        prog='gendiff',  
+        prog='gendiff',
         description='Compares two configuration files and shows a difference.'
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    
+
     parser.add_argument(
-    '-f', '--format',
-    help = 'set format of output',
-    metavar='FORMAT',    
-    choices=['plain', 'json'],
+        '-f', '--format',
+        help='set format of output',
+        metavar='FORMAT',
+        choices=['plain', 'json'],
     )
     parser.parse_args()
 
