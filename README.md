fcef26f59dac2dca091814763cf82992f524af21
иван шулубин <shulubin.ia@gmail.com>
2025-03-19 17:26:01 +0300

    Добавлено сравнение плоских файлов

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/bebcor/python-project-50/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/bebcor/python-project-50/actions)
+
+
+
+### Пример работы пакета
+
+# https://asciinema.org/a/TttQJvqMTfhhfqXkqxfK9DoB4
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,6 +7,43 @@ def read_and_parse(file_path):
         return json.load(result_file)
 
 
+
+def format_value(value):
+    if isinstance(value, bool):
+        return str(value).lower()
+    elif value is None:
+        return 'NULL'
+    else:
+        return str(value)
+
+
+def generate_diff(file_path1, file_path2):
+    data1 = read_and_parse(file_path1)
+    data2 = read_and_parse(file_path2)
+    keys = sorted(set(data1.keys()).union(set(data2.keys())))
+    diff_lines = []
+    for key in keys:
+        in_data1 = key in data1
+        in_data2 = key in data2
+        if in_data1 and not in_data2:
+            formatted_value = format_value(data1[key])
+            diff_lines.append(f"  - {key}: {formatted_value}")
+        elif in_data2 and not in_data1:
+            formatted_value = format_value(data2[key])
+            diff_lines.append(f"  + {key}: {formatted_value}")
+        else:
+            value1 = data1[key]
+            value2 = data2[key]
+            if value1 == value2:
+                formatted_value = format_value(value1)
+                diff_lines.append(f"    {key}: {formatted_value}")
+            else:
+                formatted1 = format_value(value1)
+                formatted2 = format_value(value2)
+                diff_lines.append(f"  - {key}: {formatted1}")
+                diff_lines.append(f"  + {key}: {formatted2}")
+    return '{\n' + '\n'.join(diff_lines) + '\n}'
+
 def main():
     parser = argparse.ArgumentParser(
         prog='gendiff',
@@ -21,7 +58,9 @@ def main():
         metavar='FORMAT',
         choices=['plain', 'json'],
     )
-    parser.parse_args()
+    args = parser.parse_args()
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
 
 
 if __name__ == '__main__':
